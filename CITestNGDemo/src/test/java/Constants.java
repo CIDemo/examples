593494821b7451bd698a94c64f69b2e60bbde92d
CIDemo <perfectomobile3@gmail.com>
2014-09-14 20:35:47 -0400

    Update Constants.java

--- CITestNGDemo/src/test/java/Constants.java
+++ CITestNGDemo/src/test/java/Constants.java
@@ -15,4 +15,5 @@ public static final String PM_USER = "jenkins@perfectomobile.com";
 public static final String PM_CLOUD = "demo.perfectomobile.com";
 
 public static final String REPORT_LIB = "c:\\test\\";
+public static final String REPORT_LIB_HTML = "c:/test/";
 }
