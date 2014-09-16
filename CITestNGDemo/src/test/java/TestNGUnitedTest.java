6e08fc9ec8299f25babf75a5ad780eb4c356dfe2
CIDemo <perfectomobile3@gmail.com>
2014-09-15 20:59:54 -0400

    Update TestNGUnitedTest.java

--- CITestNGDemo/src/test/java/TestNGUnitedTest.java
+++ CITestNGDemo/src/test/java/TestNGUnitedTest.java
@@ -67,6 +67,8 @@ String FileLink ="file:///" + Constants.REPORT_LIB_HTML + filename;
 Reporter.log("</br><b>Report:</b> <a href=" + FileLink + ">Perfecto Mobile Report</a>");
 
 Reporter.log("<object data=" + FileLink + " width=\"600\" height=\"400\"> <embed src="+ FileLink + " width=\"600\" height=\"400\"> </embed> Error: Embedded data could not be displayed. </object>");
+Reporter.log("<html><head><script src=\"jquery.js\"></script><script>$(function(){$(\"#includedContent\").load(" + FileLink + "); });</script> </head><body><div id=\"includedContent\"></div>  </body></html>");"
+
 
 assert rc.equals("New York/Newark, NJ (EWR)") : "Expected New York/Newark, NJ (EWR)" + rc;
 }
