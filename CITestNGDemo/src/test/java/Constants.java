06f2356c7bbd881400bb451da27ad6953885df7a
CIDemo <perfectomobile3@gmail.com>
2014-09-15 22:56:11 -0400

    Update Constants.java

--- CITestNGDemo/src/test/java/Constants.java
+++ CITestNGDemo/src/test/java/Constants.java
@@ -3,12 +3,12 @@ package test.java;
 public class Constants
 {
 /** Project Constants */
-public static final String WORK_LIB = "C:\\Jenkins_ex\\";
+//public static final String WORK_LIB = "C:\\Jenkins_ex\\";
 
-public static final String DEVICE = "4859ABC4B4F7EF8CCD9FF0C4F8A47B9FFC0B5D0A";
+//public static final String DEVICE = "4859ABC4B4F7EF8CCD9FF0C4F8A47B9FFC0B5D0A";
 
-public static final String APP_NAME = "PMIOSDemo.ipa";
-public static final String REPOSITORY_KEY = "PRIVATE:uziHTTPTest/"+APP_NAME;
+//public static final String APP_NAME = "PMIOSDemo.ipa";
+//public static final String REPOSITORY_KEY = "PRIVATE:uziHTTPTest/"+APP_NAME;
 
 public static final String PM_PASSWORD = "Perfecto123";
 public static final String PM_USER = "jenkins@perfectomobile.com";
